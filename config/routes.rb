33865256711f3e259588602ecc2d7547089ef279
Dave Kaplan <workalicious@gmail.com>
2013-01-25 12:38:40 -0600

    Week3 Day3 Challenge: about to start on the CRUD part of the exercise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rxngif::Application.routes.draw do
 
+  root :to => "Pictures#index"
+
   get "/pictures", :controller => "Pictures", :action => "index"
   get "/pictures/new", :controller => "Pictures", :action => "new"
   get "/pictures/:id", :controller => "Pictures", :action => "show"
